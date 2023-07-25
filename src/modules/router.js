2e24d9d428572e647a895f1dcc826cf1b01d287a
Antonio Bispo de Jesus Neto <antonio.neto@mundiale.com.br>
2023-07-25 20:17:15 -0300

    feat: added authentication logic

--- src/modules/router.js
+++ src/modules/router.js
@@ -1,6 +1,8 @@
 const router = require('express').Router()
 const userRouter = require('./user/userRouter')
+const sessionRouter = require('./session/sessionRouter')
 
 router.use('/user', userRouter)
+router.use('/auth', sessionRouter)
 
 module.exports = router
